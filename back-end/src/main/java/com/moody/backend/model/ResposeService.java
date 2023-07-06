3dc08a89c8f5fc4cecca6c83f27a69fc36f5ba6a
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-06 10:06:18 +0200

    feat: implemented service method to return ResponseDto

--- back-end/src/main/java/com/moody/backend/model/ResposeService.java
+++ back-end/src/main/java/com/moody/backend/model/ResposeService.java
@@ -3,6 +3,9 @@ package com.moody.backend.model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Random;
+
 @Service
 public class ResposeService {
     private final ResponseRepository repository;
@@ -13,6 +16,9 @@ public class ResposeService {
     }
 
     public ResponseDto getResponseDto(int i) {
-        return null;
+        Weather weather = repository.getWeatherByCode(i);
+        // TODO Fetch list not index and randomise the one to return
+        Genre genre = repository.getGenreByWeather(weather).get(0);
+        return new ResponseDto(weather.getDescription(), genre.getName(), genre.getMessage());
     }
 }
