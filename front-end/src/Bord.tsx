80ce755ce7bbba1572b6d5b5912604761fe9bb6f
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-07 09:40:24 +0200

    feat: implemented unspalsh image search

--- front-end/src/Bord.tsx
+++ front-end/src/Bord.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import SearchForm from './SearchForm'
 import MoodyCard from './MoodyCard'
+import { createApi } from 'unsplash-js';
+import { Photos } from 'unsplash-js/dist/methods/search/types/response';
 
 type moodyData = {
   weatherDescription: string | undefined;
@@ -12,17 +14,51 @@ type moodyData = {
 
 const Bord = () => {
   const [moody, setMoody] = useState<moodyData>();
+  const [weatherImage, setWeatherImage] = useState<string | undefined>(undefined);
+  const [genreImage, setGenreImage] = useState<string | undefined>(undefined);
+
+  const api = createApi({
+    accessKey: import.meta.env.VITE_APP_UNSPLASH_ACCESS_KEY
+  });
+
+  const fetchResult = async (query : string) : Promise<string | undefined> => {
+    // if(query == undefined) return;
+    try {
+      const data = await api.search.getPhotos({ query: query, page: 1, orientation: "landscape" });
+      if(data.response == undefined) {
+        return undefined;
+      }
+
+      var randomIndex = Math.floor(Math.random() * data.response?.results.length);
+      return data.response?.results[randomIndex].urls.small;
+    } catch(error)  {
+      console.log("something went wrong!");
+      }
+    }
+
+    useEffect(() => {
+      if(moody?.weatherDescription != undefined) {
+        fetchResult(moody?.weatherDescription).then((url) => setWeatherImage(url));
+      }
+
+      if(moody?.genreName != undefined) {
+        fetchResult(moody?.genreName).then((url) => setGenreImage(url));
+      }
+    }, [moody]);
 
   return (
     <main className='m-4'>
       <SearchForm setMoody={setMoody}/>
       <article className='my-3'>
-      <MoodyCard 
+        {moody && <MoodyCard 
                 weatherDescription={moody?.weatherDescription} 
                 genreName={moody?.genreName} 
                 genre_message={moody?.genre_message}
                 genre_upvotes={moody?.genre_upvotes}
-                id={moody?.id}/>
+                id={moody?.id}
+                weatherImgUrl={weatherImage}
+                genreImgUrl={genreImage}
+                />}
       </article>
     </main>
   )
--- front-end/src/MoodyCard.tsx
+++ front-end/src/MoodyCard.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import './MoodyCard.css'
+import { createApi } from "unsplash-js";
+import { ApiResponse } from 'unsplash-js/dist/helpers/response';
+import { Photos } from 'unsplash-js/dist/methods/search/types/response';
 
 type MoodyCardProps = {
   weatherDescription: string | undefined;
@@ -7,9 +10,11 @@ type MoodyCardProps = {
   genre_message: string | undefined;
   genre_upvotes : number | undefined;
   id : string | undefined;
+  weatherImgUrl : string | undefined;
+  genreImgUrl : string | undefined;
 }
 
-const MoodyCard = ({weatherDescription, genre_message, genreName, genre_upvotes, id} : MoodyCardProps) => {
+const MoodyCard = ({weatherDescription, genre_message, genreName, genre_upvotes, id, weatherImgUrl, genreImgUrl} : MoodyCardProps) => {
   const [upVotes, setUpVotes] = useState(0);
 
   const handleUpvote = async () => {
@@ -73,13 +78,15 @@ const MoodyCard = ({weatherDescription, genre_message, genreName, genre_upvotes,
     <>
       <div className="flip-card">
         <div className="flip-card-inner">
-          <div className="flip-card-front">
-            {/* <img src="img_avatar.png" alt="Avatar"/> */}
-            <h2>{weatherDescription}</h2>
+          <div className="flip-card-front" style={{ backgroundImage: `url('${weatherImgUrl}')` }}>
+            {/* {weatherImgUrl && <img className="flip-card-front-img" src={weatherImgUrl} alt="Avatar"/>} */}
+            <h2 className="flip-card-front-title p-3 fs-1">{weatherDescription}</h2>
           </div>
-          <div className="flip-card-back d-flex flex-column p-3 gap-1">
-            <h2>{genreName}</h2>
-            <p>{genre_message}</p>
+          <div className="flip-card-back d-flex flex-column p-3 gap-1" style={{ backgroundImage: `url('${genreImgUrl}')` }}>
+            <div className='flip-card-back-text'>
+              <h2 className='fs-1'>{genreName}</h2>
+              <p className='fs-5'>{genre_message}</p>
+            </div>
             <div className="d-flex mt-auto gap-1">
               <button onClick={handleDownvote} className='btn btn-secondary flex-grow-1'>DownVote</button>
               <p className='flex-grow-1 fs-2 m-0'>{upVotes}</p>
